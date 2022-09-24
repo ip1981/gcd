287d32d64191c7a62d9dd9c83162eac0b5db38c8
Igor Pashev <pashev.igor@gmail.com>
2022-09-24 13:13:34 +0200

    Format with Black

--- gcd.py
+++ gcd.py
@@ -3,16 +3,18 @@
 import sys
 import functools
 
+
 def gcd2(a, b):
     if b == 0:
         return a
     else:
         return gcd2(b, a % b)
 
+
 def gcdn(ns):
     return functools.reduce(gcd2, ns)
+
 
 ints = map(int, sys.argv[1:])
 gcd = gcdn(ints)
 print(gcd)
-
